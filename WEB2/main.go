9646d190010c4d19dbe41803c1adf311de007e24
soosungp33 <soosungp33@gmail.com>
2020-11-10 12:50:48 +0900

    JSON 다뤄보기

--- WEB2/main.go
+++ WEB2/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 type User struct {
-	FirstName string    `json:"first_name"` // Annotation
+	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
@@ -18,15 +18,15 @@ type fooHandler struct{}
 
 func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user := new(User)
-	err := json.NewDecoder(r.Body).Decode(user) // NewDecoder는 io.Reader를 인자로 받고 Body는 io.Reader를 포함하고 있다.
+	err := json.NewDecoder(r.Body).Decode(user)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest) // hear로 오류가 있다는 것을 알려줌
-		fmt.Fprint(w, "Bad Request: ", err)  // body에 에러를 알려줌
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request: ", err)
 		return
 	}
 	user.CreatedAt = time.Now()
 
-	data, _ := json.Marshal(user) // 인터페이스를 받아서 json형태로 바꿔주는 메소드(byte와 err를 리턴함)
+	data, _ := json.Marshal(user)
 	w.Header().Add("content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprint(w, string(data))
